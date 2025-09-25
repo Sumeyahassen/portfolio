4ebca34678a6f3d77f5f4826fda35d29e7d8decd
Your GitHub Username <sumeyahassensumi@gmail.com>
2025-09-25 03:24:25 -0700

    to include footer social mediya links

--- sumeya/src/components/Footer.jsx
+++ sumeya/src/components/Footer.jsx
@@ -5,21 +5,21 @@ function Footer() {
     <footer className="bg-gray-700 dark:bg-gray-800 text-white py-4 px-6">
       <div className="max-w-4xl mx-auto flex justify-center space-x-6">
         <a
-          href="https://www.instagram.com/yourusername"
+          href="https://www.instagram.com/sumeyahassensumeya"
           target="_blank"
           rel="noopener noreferrer"
         >
           <FaInstagram className="text-2xl hover:text-pink-500" />
         </a>
         <a
-          href="https://t.me/yourchannel"
+          href="https://t.me/SumeyaHassen"
           target="_blank"
           rel="noopener noreferrer"
         >
           <FaTelegram className="text-2xl hover:text-blue-400" />
         </a>
         <a
-          href="https://www.tiktok.com/@yourusername"
+          href="https://www.tiktok.com/@sumeya_hassen_"
           target="_blank"
           rel="noopener noreferrer"
         >
